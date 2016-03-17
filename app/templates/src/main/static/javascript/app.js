fa7f1f083b7701a74a6d601143e36959f116167e
Glenn Molnar <glenn@3wks.com.au>
2016-03-17 11:25:50 +1100

    prevent caching of ajax GET requests in older versions of IE

--- app/templates/src/main/static/javascript/app.js
+++ app/templates/src/main/static/javascript/app.js
@@ -7,12 +7,26 @@ angular.module("app", [
 	'angulartics', 
 	'angulartics.google.analytics'
 ])
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+
+    disableInternetExplorerAjaxGetCaching();
+
     $urlRouterProvider.otherwise("/");
     $stateProvider
 	    .state('home', {
 	    	url: "/",
 	    	templateUrl: "/static/templates/home.html"
 	    });
+
+    function disableInternetExplorerAjaxGetCaching() {
+        // Initialize http get header defaults if required
+        if (!$httpProvider.defaults.headers.get) {
+            $httpProvider.defaults.headers.get = {};
+        }
+
+        // Disable IE ajax request caching
+        $httpProvider.defaults.headers.get['If-Modified-Since'] = 0;
+    }
+
 });
 	
